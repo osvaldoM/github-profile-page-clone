36a3b61c77a4edb59941244ac02014e93a4ef30d
osvaldom <mekymaria@gmail.com>
2021-06-01 21:31:18 +0200

    Stop using html entities for spacing

--- js/main.js
+++ js/main.js
@@ -92,7 +92,7 @@ const renderSideBar = ({name, login, avatarUrl, bio, followers, following, starr
   return `
         <div class="profile">
         <picture class="profile__picture">
-          <img class="profile__avatar" alt="" lazy src="${avatarUrl}">
+          <img class="profile__avatar" alt="profile image" lazy src="${avatarUrl}">
            <div class="profile__status-badge">${status?.emojiHTML ?? '-'}</div>
         </picture>
         <div>
@@ -111,12 +111,12 @@ const renderSideBar = ({name, login, avatarUrl, bio, followers, following, starr
                 <path fill-rule="evenodd"
                       d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z"></path>
               </svg>
-              <strong class="profile__info-count">${followers.totalCount ?? 0}</strong> followers &nbsp;·
+              <strong class="profile__info-count">${followers.totalCount ?? 0}</strong> followers
             </a>
           </li>
           <li class="profile__info-item">
             <a class="profile__info-link">
-              <strong class="profile__info-count">${following.totalCount ?? 0} </strong> following &nbsp;·
+              <strong class="profile__info-count">${following.totalCount ?? 0} </strong> following
             </a>
           </li>
           <li class="profile__info-item">
