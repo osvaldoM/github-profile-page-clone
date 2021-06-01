30554ce15f2fd153c06234f3f68a6b65cd996271
osvaldom <mekymaria@gmail.com>
2021-06-01 16:42:37 +0200

    Toggle navbar

--- js/main.js
+++ js/main.js
@@ -178,3 +178,15 @@ document.querySelector('.search-user-form').addEventListener('submit', event => 
     loadingSpinner.classList.add('hidden');
   });
 });
+
+/*
+ * Handle toggle menu
+ */
+let isExpanded = false;
+const toggleButton = document.querySelector('.main-nav__toggle-nav');
+
+toggleButton.addEventListener('click', () => {
+  isExpanded = !isExpanded;
+  toggleButton.setAttribute('aria-expanded', isExpanded);
+  toggleButton.closest('.main-nav__list').classList.toggle('expanded');
+});
