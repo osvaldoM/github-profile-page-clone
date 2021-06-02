75369d0fb7ae22008811d368f8d3b48bc90d7fa1
osvaldom <mekymaria@gmail.com>
2021-06-02 23:47:46 +0200

    Load sample data

--- js/main.mjs
+++ js/main.mjs
@@ -2,31 +2,38 @@ import renderSideBar from './templates/sidebarTemplate.mjs'
 import renderRepositoryList from './templates/repositoryListTemplate.mjs'
 import {fetchUser, isAuthenticated, authenticate} from "./userService.mjs";
 
-document.querySelector('.search-user-form').addEventListener('submit', event => {
-  event.preventDefault();
-  event.stopImmediatePropagation();
-
-  if(!isAuthenticated()){
-    authenticate(prompt('Please insert a Github Personal Access token, you can generate a token at: https://github.com/settings/tokens'));
-  }
-
-  const loadingSpinner = event.target.querySelector('.loading-spinner');
-  loadingSpinner.classList.remove('hidden');
-
-  const userName = event.target.user_name.value;
-
-  fetchUser(userName).then(userData => {
+const renderUserData = (userData) => {
     document.querySelector('.sidebar').innerHTML = renderSideBar(userData.data.user);
     document.querySelector('.repository__list').innerHTML = renderRepositoryList(userData.data.user.repositories.nodes);
     document.querySelector('.repositories-count').textContent = userData.data.user.repositories.totalCount;
     document.title = `${userData.data.user.login}(${userData.data.user.name})`;
-    document.querySelector('.search-user-banner').classList.add('hidden');
-  }).catch(err => {
-    alert('there has been an error, see the console for more details')
-    console.log(err);
-  }).finally(() => {
-    loadingSpinner.classList.add('hidden');
-  });
+}
+
+import('./sampleUser.mjs')
+    .then(res => renderUserData(res.sampleUser))
+    .catch(error => {
+        console.log(error);
+    });
+
+document.querySelector('.search-user-form').addEventListener('submit', event => {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+
+    if(!isAuthenticated()){
+        authenticate(prompt('The github Graphq API requires an authenticated user. Please insert a Github Personal Access token, you can generate a token at: https://github.com/settings/tokens'));
+    }
+
+    const loadingSpinner = event.target.querySelector('.loading-spinner');
+    loadingSpinner.classList.remove('hidden');
+
+    const userName = event.target.user_name.value;
+
+    fetchUser(userName).then(renderUserData).catch(err => {
+        alert('there has been an error, see the console for more details')
+        console.log(err);
+    }).finally(() => {
+        loadingSpinner.classList.add('hidden');
+    });
 });
 
 /*
@@ -36,16 +43,16 @@ let isExpanded = false;
 const toggleButton = document.querySelector('.main-nav__toggle-nav');
 
 toggleButton.addEventListener('click', () => {
-  isExpanded = !isExpanded;
-  toggleButton.setAttribute('aria-expanded', isExpanded);
-  toggleButton.setAttribute('aria-label', isExpanded ? 'Close navigation' : 'Open navigation');
-  toggleButton.closest('.main-nav__list').classList.toggle('expanded');
+    isExpanded = !isExpanded;
+    toggleButton.setAttribute('aria-expanded', isExpanded);
+    toggleButton.setAttribute('aria-label', isExpanded ? 'Close navigation' : 'Open navigation');
+    toggleButton.closest('.main-nav__list').classList.toggle('expanded');
 });
 
 document.addEventListener('keydown', (event) => {
-  if (event.code === "Slash"){
-    event.preventDefault();
-    event.stopImmediatePropagation();
-    document.querySelector('.user-name-input').focus();
-  }
+    if(event.code === "Slash"){
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        document.querySelector('.user-name-input').focus();
+    }
 });
---
+++ js/sampleUser.mjs
@@ -0,0 +1,313 @@
+const sampleUser = {
+    "data": {
+        "user": {
+            "avatarUrl": "https://avatars.githubusercontent.com/u/11247633?u=2be4a600f089eb606ad83f6ebdc59892d7d6fb90&v=4",
+            "bio": "Pathological keen learner, with ♥ for crafting beautiful front end experiences.\r\n\r\nOccasional blogger",
+            "name": "Osvaldo Maria",
+            "login": "osvaldoM",
+            "followers": {
+                "totalCount": 58
+            },
+            "following": {
+                "totalCount": 46
+            },
+            "starredRepositories": {
+                "totalCount": 191
+            },
+            "status": {
+                "emojiHTML": "<div><g-emoji class=\"g-emoji\" alias=\"dart\" fallback-src=\"https://github.githubassets.com/images/icons/emoji/unicode/1f3af.png\">🎯</g-emoji></div>",
+                "message": "Focusing"
+            },
+            "repositories": {
+                "totalCount": 40,
+                "nodes": [
+                    {
+                        "url": "https://github.com/osvaldoM/github-profile-page-clone",
+                        "name": "github-profile-page-clone",
+                        "description": "clone of github user profile page",
+                        "updatedAt": "2021-06-02T12:50:25Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#e34c26",
+                            "name": "HTML"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/devnotes",
+                        "name": "devnotes",
+                        "description": "notes from research and tests",
+                        "updatedAt": "2021-05-27T21:42:36Z",
+                        "stargazerCount": 3,
+                        "forkCount": 1,
+                        "primaryLanguage": {
+                            "color": "#4F5D95",
+                            "name": "PHP"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/my-ui-components",
+                        "name": "my-ui-components",
+                        "description": "Collection of reusable ui components built with vue/react and tailwindcss",
+                        "updatedAt": "2021-05-12T10:27:56Z",
+                        "stargazerCount": 2,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/osvaldom.github.io",
+                        "name": "osvaldom.github.io",
+                        "description": "Personal website on github",
+                        "updatedAt": "2021-05-11T22:52:40Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#41b883",
+                            "name": "Vue"
+                        },
+                        "licenseInfo": {
+                            "nickname": "GNU GPLv3"
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/react-task-tracker",
+                        "name": "react-task-tracker",
+                        "description": "learning react hooks, suspense and concurrent mode",
+                        "updatedAt": "2021-05-07T12:42:08Z",
+                        "stargazerCount": 1,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/sentry-javascript",
+                        "name": "sentry-javascript",
+                        "description": "Official Sentry SDKs for Javascript",
+                        "updatedAt": "2021-04-26T15:18:40Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#2b7489",
+                            "name": "TypeScript"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/mailersend-php",
+                        "name": "mailersend-php",
+                        "description": "MailerSend PHP SDK",
+                        "updatedAt": "2021-04-26T15:10:14Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#4F5D95",
+                            "name": "PHP"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/gridsome-plugin-gtag",
+                        "name": "gridsome-plugin-gtag",
+                        "description": null,
+                        "updatedAt": "2021-04-23T22:13:02Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/laravel-hotel",
+                        "name": "laravel-hotel",
+                        "description": "laravel hotel booking demo app",
+                        "updatedAt": "2021-04-13T21:56:09Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#4F5D95",
+                            "name": "PHP"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/minisend",
+                        "name": "minisend",
+                        "description": "minisend",
+                        "updatedAt": "2021-04-11T16:15:27Z",
+                        "stargazerCount": 4,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#4F5D95",
+                            "name": "PHP"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/svgomg",
+                        "name": "svgomg",
+                        "description": "Web GUI for SVGO",
+                        "updatedAt": "2021-03-01T12:30:52Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/data-studio-rest-api-connector",
+                        "name": "data-studio-rest-api-connector",
+                        "description": "Google data studio connector that fetches data from api endpoint",
+                        "updatedAt": "2021-02-26T20:01:05Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/viewerjs",
+                        "name": "viewerjs",
+                        "description": "JavaScript image viewer.",
+                        "updatedAt": "2021-02-25T14:45:18Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/screeps-bot",
+                        "name": "screeps-bot",
+                        "description": "simple bot for https://screeps.com",
+                        "updatedAt": "2021-02-25T14:45:18Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/mpesa-demo",
+                        "name": "mpesa-demo",
+                        "description": "App for demoing an integration with the m-pesa API",
+                        "updatedAt": "2021-02-25T14:45:18Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": {
+                            "nickname": "GNU GPLv3"
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/ngularDemoApp",
+                        "name": "ngularDemoApp",
+                        "description": "app for learning angular 2 ",
+                        "updatedAt": "2021-02-25T14:45:18Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#2b7489",
+                            "name": "TypeScript"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/m-pesa-test",
+                        "name": "m-pesa-test",
+                        "description": "Demo application for testing mpesa API",
+                        "updatedAt": "2021-02-25T14:45:18Z",
+                        "stargazerCount": 5,
+                        "forkCount": 3,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": null
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/ntxuva-7.x-1.0",
+                        "name": "ntxuva-7.x-1.0",
+                        "description": "Current version of ntxuva. The software platform for MOPA.co.mz",
+                        "updatedAt": "2021-02-25T14:45:18Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#4F5D95",
+                            "name": "PHP"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/playground",
+                        "name": "playground",
+                        "description": "Lab for my Experiments.",
+                        "updatedAt": "2021-02-25T14:45:18Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#f1e05a",
+                            "name": "JavaScript"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    },
+                    {
+                        "url": "https://github.com/osvaldoM/Moovi",
+                        "name": "Moovi",
+                        "description": "Moovi projects using webcomponents",
+                        "updatedAt": "2021-02-25T14:45:18Z",
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "primaryLanguage": {
+                            "color": "#e34c26",
+                            "name": "HTML"
+                        },
+                        "licenseInfo": {
+                            "nickname": null
+                        }
+                    }
+                ]
+            }
+        }
+    }
+}
+export {
+    sampleUser
+}
